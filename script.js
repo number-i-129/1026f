6e9c4c712ada22193ee026c4a50d69a0f3f92736
number-i-129 <mymelomxs@gmail.com>
2024-10-17 00:11:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -21,13 +21,3 @@ window.onload = function() {
     setInterval(updateTime, 1000);
     updateTime();
 };
-
-window.onload = function() {
-    function updateTime() {
-        const now = new Date();
-        const timeString = now.toLocaleTimeString('ja-JP', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
-        document.querySelector('.time').textContent = timeString;
-    }
-    setInterval(updateTime, 1000);
-    updateTime();
-};
